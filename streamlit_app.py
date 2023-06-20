75193d15b58f7dfd6f226ee9ab25dc966c7fd860
Ahmed Mostafa Mohamed <61332951+AhmedMostafa-notabot@users.noreply.github.com>
2023-06-20 15:27:34 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,16 +15,27 @@ def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
 #   llm = GPT4All(model="./models/gpt4all-model.bin", n_ctx=512, n_threads=8)
   st.info(llm(input_text))
+def generate_response2(input_text):
+#   llm = GPT4All(model="./models/gpt4all-model.bin", n_ctx=512, n_threads=8)
+  st.info(pdf_qa({"question": input_text})['answer'])
 
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
   if not openai_api_key.startswith('sk-'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if uploaded_file_pdf:
+  if uploaded_file_pdf[0]:
     path=uploaded_file_pdf[0].read()
     loader = PyPDFLoader(path)
     pages = loader.load_and_split()
+    embeddings = OpenAIEmbeddings()
+    vectordb = Chroma.from_documents(pages, embedding=embeddings, 
+                                     persist_directory=".")
+    vectordb.persist()
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    pdf_qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0.8) , vectordb.as_retriever(), memory=memory)
   if submitted and openai_api_key.startswith('sk-'):
     print(text)
     generate_response(text)
+  if uploaded_file_pdf[0] and submitted and openai_api_key.startswith('sk-'):
+    generate_response2(text)
