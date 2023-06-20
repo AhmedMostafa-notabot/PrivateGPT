1056819566cdfe60fbdab725df7c61ccb1b07132
Ahmed Mostafa Mohamed <61332951+AhmedMostafa-notabot@users.noreply.github.com>
2023-06-20 13:50:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ st.title('🦜 VNCR-GPT')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key!')
 uploaded_file_pdf = st.sidebar.file_uploader("Upload PDF Files",type=["pdf"], accept_multiple_files=True)
-print(uploaded_file_pdf)
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
 #   llm = GPT4All(model="./models/gpt4all-model.bin", n_ctx=512, n_threads=8)
@@ -17,6 +16,9 @@ with st.form('my_form'):
   submitted = st.form_submit_button('Submit')
   if not openai_api_key.startswith('sk-'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if uploaded_file_pdf:
+    path=uploaded_file_pdf.read()
+    print(path)
   if submitted and openai_api_key.startswith('sk-'):
     print(text)
     generate_response(text)
