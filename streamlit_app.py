5752d2fc928c4942152543cdaa8957468bbf1fd2
Ahmed Mostafa Mohamed <61332951+AhmedMostafa-notabot@users.noreply.github.com>
2023-06-23 02:02:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,15 +37,13 @@ def preprocess(text):
 
 def summarize_text(text):
 
-  prompt =   [{"role": "user", "content": f"Summarize this in 4 sentences:\n{text}"}]
+  prompt =   [{"role": "user", "content": f"Summarize this in 5 sentences:\n{text}"}]
   response = openai.ChatCompletion.create(
       model="gpt-3.5-turbo-16k", 
       messages=prompt,
       temperature=0.2, 
       max_tokens=100,
-      top_p=0.9, 
-      frequency_penalty=1,
-      presence_penalty=0
+      top_p=0.9
   )
 
   return response["choices"][0]['message']['content']
@@ -60,7 +58,7 @@ with st.form('my_form'):
     sumtext=[]
     pdf = PdfReader(uploaded_file_pdf)
     pages=pdf.pages
-    minstep=min(len(pages),40)
+    minstep=min(len(pages),20)
     for i in pages:
       text.append(preprocess(i.extract_text()))
     for i in range(0,len(text),minstep):
