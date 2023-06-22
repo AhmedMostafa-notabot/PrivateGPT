6321d76b81dc4cea7b67133c14d8b4494f866eac
Ahmed Mostafa Mohamed <61332951+AhmedMostafa-notabot@users.noreply.github.com>
2023-06-23 01:22:59 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from PyPDF2 import PdfReader
 from langchain.document_loaders import PyPDFLoader 
 from langchain.embeddings import OpenAIEmbeddings 
 from langchain.vectorstores import Chroma 
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import RetrievalQA
 from langchain.memory import ConversationTokenBufferMemory
 from langchain.llms import OpenAI
 
@@ -24,7 +24,8 @@ def generate_response(input_text):
 def generate_response2(input_text):
   # print(input_text)
 #   llm = GPT4All(model="./models/gpt4all-model.bin", n_ctx=512, n_threads=8)
-  out=pdf_qa({'question': str(input_text)})['answer']
+  # out=pdf_qa({'question': str(input_text)})['answer']
+  out=pdf_qa.run(str(input_text))
   st.info(out)
   # history=[{str(input_text),out}]
   # return history
@@ -41,7 +42,7 @@ def summarize_text(text):
       model="gpt-3.5-turbo", 
       messages=prompt,
       temperature=0.2, 
-      max_tokens=100, # = 112 words
+      max_tokens=100,
       top_p=0.9, 
       frequency_penalty=1,
       presence_penalty=0
@@ -59,7 +60,7 @@ with st.form('my_form'):
     sumtext=[]
     pdf = PdfReader(uploaded_file_pdf)
     pages=pdf.pages
-    minstep=min(len(pages),11)
+    minstep=min(len(pages),15)
     for i in pages:
       text.append(preprocess(i.extract_text()))
     for i in range(0,len(text),minstep):
@@ -72,9 +73,9 @@ with st.form('my_form'):
                                      persist_directory=".")
     # vectordb.persist()
     # chat_history=[]
-    memory = ConversationTokenBufferMemory(memory_key="chat_history", return_messages=True ,llm=OpenAI(temperature=0.4,model_name='gpt-3.5-turbo-16k'))
-    pdf_qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0,model_name='gpt-3.5-turbo-16k',max_tokens=135,frequency_penalty=1,presence_penalty=0),
-                                                   vectordb.as_retriever(search_type='similarity',search_kwargs={"k":1}),memory=memory)
+    # memory = ConversationTokenBufferMemory(memory_key="chat_history", return_messages=True ,llm=OpenAI(temperature=0.4,model_name='gpt-3.5-turbo-16k'))
+    pdf_qa = RetrievalQA.from_chain_type(OpenAI(temperature=0,model_name='gpt-3.5-turbo-16k',max_tokens=135,frequency_penalty=1,presence_penalty=0),
+                                                   vectordb.as_retriever(search_type='similarity',search_kwargs={"k":1}))
     # ,memory=ConversationBufferWindowMemory(memory_key="chat_history",k=1,return_messages=True)
   else:
     try:
