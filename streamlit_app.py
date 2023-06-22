d8b6d360945553a5520542e7fd11b3ffaa99c5fb
Ahmed Mostafa Mohamed <61332951+AhmedMostafa-notabot@users.noreply.github.com>
2023-06-22 23:29:09 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,8 @@ def generate_response(input_text):
 def generate_response2(input_text):
   # print(input_text)
 #   llm = GPT4All(model="./models/gpt4all-model.bin", n_ctx=512, n_threads=8)
-  out=pdf_qa({'question': str(input_text)})['answer']
+  # out=pdf_qa({'question': str(input_text)})['answer']
+  out=pdf_qa.run(str(input_text))
   st.info(out)
   # history=[{str(input_text),out}]
   
