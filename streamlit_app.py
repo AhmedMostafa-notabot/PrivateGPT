c727ff8e1b472685d5d793210b44de9269b5b940
Ahmed Mostafa Mohamed <61332951+AhmedMostafa-notabot@users.noreply.github.com>
2023-06-26 21:05:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ def generate_response(input_text):
 def generate_response2(input_text):
   out=pdf_qa({"query": str(input_text)})
   res=out['result']
-  ref=''.join(["\n "+ "Source: \n" + i.metadata['source']+"\n" +"\n Content:"+"\n\n"+ i.page_content for i in out['source_documents']])
+  ref=''.join(["\n\n "+ "Source: \n" + i.metadata['source']+"\n" +"\n Content:"+"\n\n"+ i.page_content for i in out['source_documents']])
   st.info(res+' \n \n '+"Reference: \n "+ref)
   
 
