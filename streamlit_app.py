fbdc01039d842b6f5b76e3f47270df2db8b99b95
Ahmed Mostafa Mohamed <61332951+AhmedMostafa-notabot@users.noreply.github.com>
2023-09-02 12:56:02 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ from streamlit_chat import message
 st.title('🦜 VNCR-GPT')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key!',type="password")
-uploaded_file_pdf = st.sidebar.file_uploader("Upload PDF Files",type=["pdf"],accept_multiple_files=True)
+uploaded_file_pdf = st.sidebar.file_uploader("Upload PDF Files",type=["pdf","docx","doc"],accept_multiple_files=True)
 # uploaded_file_pdf2 = st.sidebar.file_uploader("Upload PDF Files For 2nd Side Of Debate",type=["pdf"],accept_multiple_files=True)
 embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
 
