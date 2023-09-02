247b20df5755875b987b3e3946596b95cebba061
Ahmed Mostafa Mohamed <61332951+AhmedMostafa-notabot@users.noreply.github.com>
2023-09-02 15:26:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,6 +90,7 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            .viewerBadge_container__r5tak styles_viewerBadge__CvC9N {display:none !important;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
