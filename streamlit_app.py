c50ed6bb340750e1f93cc3f764f5eba734688c73
Ahmed Mostafa Mohamed <61332951+AhmedMostafa-notabot@users.noreply.github.com>
2023-09-02 13:43:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.title(':sunglasses: PrivateGPT')
 
 st.sidebar.title('DocumentGPT')
 openai_api_key = st.sidebar.text_input('First Insert OpenAI API Key',type="password")
-uploaded_file_pdf = st.sidebar.file_uploader("Second Upload Documents",type=["pdf","docx","doc"],accept_multiple_files=True)
+uploaded_file_pdf = st.sidebar.file_uploader("Then Upload Documents",type=["pdf","docx","doc"],accept_multiple_files=True)
 # uploaded_file_pdf2 = st.sidebar.file_uploader("Upload PDF Files For 2nd Side Of Debate",type=["pdf"],accept_multiple_files=True)
 embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
 
