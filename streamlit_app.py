1013c61269ff7642746c21f178d43b2426088999
Ahmed Mostafa Mohamed <61332951+AhmedMostafa-notabot@users.noreply.github.com>
2023-09-02 13:25:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.llms import OpenAI
 from streamlit_chat import message
 
-st.title('🦜:sunglasses: VNCR-GPT')
+st.title(':sunglasses: Private-GPT')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key!',type="password")
 uploaded_file_pdf = st.sidebar.file_uploader("Upload Documents",type=["pdf","docx","doc"],accept_multiple_files=True)
